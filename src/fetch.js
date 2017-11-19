42d3397d2f129b3d6e1f5e4e7e25d516d12837b2
Eduardo Campaña <asrielo@gmail.com>
2017-11-19 16:10:01 +0100

    WIP Building Root schema

--- src/fetch.js
+++ src/fetch.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 const util = require('util');
 const parseXML = util.promisify(require('xml2js').parseString);
 
-fetch('http://boe.es/diario_boe/xml.php?id=BOE-S-20171117')
+fetch(`http://boe.es/diario_boe/xml.php?id=BOE-S-${20171118}`)
   .then(res => res.text())
-  .then(text => parseXML(text))
-  .then(data => console.dir(data));
+  .then(parseXML)
+  .then(data => console.log(data.sumario.diario[0].sumario_nbo[0].urlPdf));
--- src/schema.js
+++ src/schema.js
@@ -1,13 +1,53 @@
-const { buildSchema } = require('graphql');
+const {
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLNonNull,
+} = require('graphql');
 
-const schema = buildSchema(/* GraphQL */ `
-  type Query {
-    hello: String
-  }
-`);
+const MetaType = new GraphQLObjectType({
+  name: 'Meta',
+  fields: () => ({
+    pub: { type: new GraphQLNonNull(GraphQLString) },
+    fecha: { type: new GraphQLNonNull(GraphQLString) },
+    fechaAnt: { type: new GraphQLNonNull(GraphQLString) },
+    fechaSig: { type: GraphQLString },
+  }),
+});
 
-const rootValue = {
-  hello: () => 'Hello World!',
-};
+const DiarioType = new GraphQLObjectType({
+  name: 'Diario',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    num: { type: new GraphQLNonNull(GraphQLInt) },
+    pdf: { type: PDFType },
+  }),
+});
 
-module.exports = { schema, rootValue };
+const PDFType = new GraphQLObjectType({
+  name: 'PDF',
+  fields: () => ({
+    sizeBytes: new GraphQLNonNull(GraphQLInt),
+    sizeKBytes: new GraphQLNonNull(GraphQLInt),
+    url: new GraphQLNonNull(GraphQLString),
+  }),
+});
+
+const SumarioType = new GraphQLObjectType({
+  name: 'Sumario',
+  fields: () => ({
+    meta: { type: MetaType },
+    diario: { type: new GraphQLList(DiarioType) },
+  }),
+});
+
+const Root = new GraphQLObjectType({
+  name: 'Root',
+  fields: () => ({
+    sumarios: new GraphQLList(SumarioType),
+  }),
+});
+
+module.exports = schema;
